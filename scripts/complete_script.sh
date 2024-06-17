16aa4207c7e318cce129529cafa8021bda10c18a
Devrim Yugnuk <devrimyugnuk@gmail.com>
2024-06-17 14:44:34 +0200

    Updated the complete script so that the original video data can be given.

--- scripts/complete_script.sh
+++ scripts/complete_script.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-
-#TODO: The user has to provide the path to the original video, the video logs, the ocr logs, and the overlay image.
-
 # -------- User Input Section ----------
 # Check if the user has provided the path to the videos folder
 if [ -z "$1" ]; then
@@ -16,12 +13,40 @@ if [ -z "$2" ]; then
     exit 1
 fi
 
+# Check if the user has provided the path to the original video
+if [ -z "$3" ]; then
+    echo "Please provide the path to the original video"
+    exit 1
+fi
+
+# Check if the user has provided the path to the video logs
+if [ -z "$4" ]; then
+    echo "Please provide the path to the video logs"
+    exit 1
+fi
+
+# Check if the user has provided the path to the OCR logs
+if [ -z "$5" ]; then
+    echo "Please provide the path to the OCR logs"
+    exit 1
+fi
+
+# Check if the user has provided the path to the overlay image
+if [ -z "$6" ]; then
+    echo "Please provide the path to the overlay image"
+    exit 1
+fi
+
 # Get the list of available simulations from the simulator
 SIMULATIONS=$(python3 -u simulator/simulator.py | grep "Available simulations" | cut -d ":" -f 2)
 
 # Save the user input in variables
 VIDEOS_FOLDER=$1
 SIMULATION=$2
+ORIGINAL_VIDEO=$3
+VIDEO_LOGS=$4
+OCR_LOGS=$5
+OVERLAY_IMAGE=$6
 
 # Check if the simulation provided by the user is valid
 if ! echo "$SIMULATIONS" | grep -q "$SIMULATION"; then
@@ -66,7 +91,7 @@ echo "RTSP server running..."
 
 # -------- Validator Section ----------
 
-python3 validator/validator.py video_folder video_logs ocr_logs overlay_image.png 0
+python3 validator/validator.py $VIDEOS_FOLDER $VIDEO_LOGS $OCR_LOGS $OVERLAY_IMAGE 0
 
 # -------- Cleanup Section ----------
 
